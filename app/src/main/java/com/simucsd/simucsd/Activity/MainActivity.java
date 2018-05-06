800c43fcefa849be89ce0c6d57b667910e7876c2
Alex <alexcai1226@outlook.com>
2018-05-05 19:09:31 -0700

    hi

--- app/src/main/java/com/simucsd/simucsd/Activity/MainActivity.java
+++ app/src/main/java/com/simucsd/simucsd/Activity/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 import com.simucsd.simucsd.R;
 
-public class MainActivity extends Activity{
+public class MainActivity extends Activity               {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
